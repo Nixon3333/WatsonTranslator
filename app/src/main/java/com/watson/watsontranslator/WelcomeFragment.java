d8818f96cc3aa58b30f2a5cb7135729c734c2a47
Nikita Drygin <nixon3333@gmail.com>
2019-02-06 22:44:13 +0700

    Add WelcomeFragment and cosmetics

---
+++ app/src/main/java/com/watson/watsontranslator/WelcomeFragment.java
@@ -0,0 +1,27 @@
+package com.watson.watsontranslator;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class WelcomeFragment extends Fragment {
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.welcome_fragment, container, false);
+
+    }
+}
